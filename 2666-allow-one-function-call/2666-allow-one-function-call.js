74b458e1ba958acd148831bb595faf87eb8e50f1
Harmeet Singh <130365071+harmeetsingh11@users.noreply.github.com>
2023-05-12 16:14:33 +0530

    Time: 59 ms (58.02%), Space: 42.1 MB (30.84%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -0,0 +1,24 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function (fn) {
+  let call = false;
+  return function (...args) {
+    if (!call) {
+      call = true;
+      const result = fn(...args);
+      return result;
+    } else {
+      return undefined;
+    }
+  };
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
