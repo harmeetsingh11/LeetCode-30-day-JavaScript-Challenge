ac2737ab1c41f11a8295ea1e4ef5884c8bb71df7
Harmeet Singh <130365071+harmeetsingh11@users.noreply.github.com>
2023-05-22 21:35:49 +0530

    Time: 111 ms (14.35%), Space: 77.1 MB (6.83%) - LeetHub

---
+++ 2633-convert-object-to-json-string/2633-convert-object-to-json-string.js
@@ -0,0 +1,38 @@
+/**
+ * @param {any} object
+ * @return {string}
+ */
+var jsonStringify = function (object) {
+  if (object === null || object === undefined) {
+    return String(object);
+  }
+
+  //   Arrays []
+  if (Array.isArray(object)) {
+    // map each item in the array to make recursive call, return new array
+    const result = object.map((item) => jsonStringify(item));
+    // console.log(result); ["1","2","3","4","5","6","7","8]
+    return `[${result.join(',')}]`; // array wrapped with [] with , between values. return [1,2,3,45,6]
+  }
+
+  // Objects {}
+  if (typeof object === 'object') {
+    // get keys of the object
+    const getKeys = Object.keys(object);
+    // console.log(getKeys);
+    // map each key with value of object to make recursive call, return new array
+    const keyValPairs = getKeys.map(
+      (key) => `"${key}":${jsonStringify(object[key])}`
+    );
+    // console.log(keyValPairs); [ '"a":1', '"b":2', '"c":3', '"d":4', '"e":5' ]
+    return `{${keyValPairs.join(',')}}`; // object wrapped with {} with , between key-values pairs
+  }
+
+  //   Strings
+  if (typeof object === 'string') {
+    return `"${String(object)}"`;
+  }
+
+  //   booleans, numbers
+  return String(object);
+};
