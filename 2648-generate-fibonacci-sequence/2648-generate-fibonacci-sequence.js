2749f62b73abc92a171c456d8f5cf94c17e1225f
Harmeet Singh <130365071+harmeetsingh11@users.noreply.github.com>
2023-06-04 11:01:56 +0530

    Time: 52 ms (85.16%), Space: 42.6 MB (9.22%) - LeetHub

---
+++ 2648-generate-fibonacci-sequence/2648-generate-fibonacci-sequence.js
@@ -0,0 +1,20 @@
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function* () {
+  let num1 = 0,
+    num2 = 1;
+
+  while (true) {
+    yield num1;
+    // Parallel Assignment with destructuring
+    [num1, num2] = [num2, num1 + num2];
+  }
+};
+
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
